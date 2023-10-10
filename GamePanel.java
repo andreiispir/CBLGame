622d38bd76523a8df4aeaa57f8801e623db0f1bf
Lia Banuta <lia.banuta@gmail.com>
2023-10-10 16:05:16 +0200

    replaced background

--- GamePanel.java
+++ GamePanel.java
@@ -28,7 +28,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         characterRect = new Rectangle(characterX, characterY, 50, 50);
 
         // Load background image
-        backgroundImage = new ImageIcon("page.jpg").getImage(); // Change to your background image path
+        backgroundImage = new ImageIcon("background.png").getImage(); // Change to your background image path
 
         // Add the KeyListener to the game panel
         addKeyListener(this);
